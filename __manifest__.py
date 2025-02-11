946222a80204a29ddd53e9158c5a48d8ddcf5e24
Pablo Muiño <pabmuirod@gmail.com>
2025-02-11 08:52:33 +0100

    Editado archivo __manifesst__.py

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Alquiler de Productos',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Alquiler de productos de una empresa.',
+    'description': """
+    Módulo para gestionar el alquiler de productos de una empresa.
+    Permite asignar préstamos incluyendo: un cliente, un producto, 
+    una fecha de inicio, una fecha final, un estado y una observación.
+    """,
+    'author': 'Pablo Muiño',
+    'depends': ['base', 'sale', 'contacts'], # Requiere módulos base, ventas y contactos
+    'data': [
+    'views/alquiler_productos_views.xml',
+    'security/ir.model.access.csv',
+    ],
+    'icon': '/alquiler_productos/static/description/icon56.png',
+    'installable': True,
+    'application': True,
+}
